0e18d3f543bfaef409cfbfe2bd63b3d4e615bacb
追梦人 <128029849+lndsqhj@users.noreply.github.com>
2025-05-08 17:56:29 +0800

    Update dandanju.js

--- vod/js/dandanju.js
+++ vod/js/dandanju.js
@@ -11,7 +11,7 @@ import { } from '../../core/uzUtils.js'
 
 
 const appConfig = {
-    _webSite: 'https://www.dandanju.tv',
+    _webSite: 'https://www.dandanju.art',
     headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
     },
